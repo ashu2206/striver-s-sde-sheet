b6b3baf1be478c7efb5f1df6a0a28bee3c8ab72e
Ashu Baghel <102874744+ashu2206@users.noreply.github.com>
2023-01-02 00:03:36 +0530

    Create 2_Sort Colors.java

---
+++ 2_Sort Colors.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        
+    int len=nums.length;
+    int low=0;
+    int mid=0;
+    int high=nums.length-1;
+    int temp;
+   while(mid<=high)
+        {
+            if(nums[mid]==0)
+            {
+                temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                mid++;
+                low++;
+            }
+            else if(nums[mid]==1)
+            {
+                mid++;
+                
+            }
+            else {
+                temp=nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=temp;
+                high--;
+            }
+            
+        }
+    }
+
+       
+        
+    
+}
